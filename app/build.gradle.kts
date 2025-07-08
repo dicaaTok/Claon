60a0fb656754ef408555f8f6d1526da885a8a7b0
dicaaTok <dicaaGeek@gmail.com>
2025-07-08 20:12:22 +0600

    5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 }
 
 android {
-    namespace = "com.dica.claon"
+    namespace = "com.dica.Ciaon"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.dica.claon"
-        minSdk = 28
+        applicationId = "com.dica.Ciaon"
+        minSdk = 35
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -27,11 +27,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
     buildFeatures {
         viewBinding = true
@@ -60,5 +60,4 @@ dependencies {
     //    NavGraph
     implementation ("androidx.navigation:navigation-fragment:$nav_version")
     implementation (libs.androidx.navigation.ui)
-
 }
